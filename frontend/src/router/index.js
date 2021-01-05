9ae51fb6c670b22632eb8ddf088b32263440cab2
Victor <victortornbom@gmail.com>
2021-01-05 17:50:55 +0100

    updated db

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,17 +1,21 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import HomePage from '../pages/HomePage'
 
 Vue.use(VueRouter)
 
 const routes = [
-
+    {
+        path: "/",
+        name: "HomePage",
+        components: HomePage,
+    },
 ]
 
 const router = new VueRouter({
     routes,
     mode: "history",
     base: process.env.BASE_URL,
-    routes,
 });
 
 export default router;
