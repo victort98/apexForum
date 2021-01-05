a53744b6dd182905c2d06891b75f9d34494781d8
Victor <victortornbom@gmail.com>
2021-01-05 14:15:58 +0100

    setting up vue frontend

---
+++ frontend/vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    devServer: {
+      proxy:
+      {
+        "^/api/": { target: "http://localhost:3000", ws: true, changeOrigin: true },
+        "^/auth/": { target: "http://localhost:3000", ws: true, changeOrigin: true },
+      }
+    }
+  }
